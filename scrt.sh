c036e3a6c93d1afcf1ed46e6bdbc7c404a82db50
Jingxuan Wei <joel@jp.ibm.com>
2018-05-16 13:36:06 +0900

    second commit

--- scrt.sh
+++ scrt.sh
@@ -1,15 +1,20 @@
 #!/bin/bash
 
-TIMESTAMP=$(date +"%Y%m%d%H%M")
+# Create timestamp for apending filenames
+TIMESTAMP=$(date +"%Y%m%d")
 
-# 接続先情報
+# SSH Connection Info
 SSH_USER=root
 SSH_PASS=default
-SSH_HOST=10.128.1.145
-SSH_HOSTNAME=testapm1
-LOG_PATH=~/Desktop/
-REMOTE_CMD_CPU="tmsh show sys cpu"
-REMOTE_CMD_MEMORY="tmsh show sys memory"
+SSH_HOSTNAMES=('AFTVP03' 'AFTVP04')
+SSH_HOSTS=('10.128.1.145' '10.128.1.145')
+
+# Specify Log Directory
+LOG_PATH=~/Desktop/var/log
+
+# Commands for checking cpu & memory usages
+REMOTE_CMD_CPU="date; tmsh show sys cpu"
+REMOTE_CMD_MEMORY="date; tmsh show sys memory"
 
 
 # 後述のSSH_ASKPASSで設定したプログラム(本ファイル自身)が返す内容
@@ -18,15 +23,20 @@ if [ -n "$PASSWORD" ]; then
   exit 0
 fi
 
-# SSH_ASKPASSで呼ばれるシェルにパスワードを渡すために変数を設定
+# Prepare a PASSWORD for the SSH_ASKPASS-shell
 export PASSWORD=$SSH_PASS
 
-# SSH_ASKPASSに本ファイルを設定
+# Set SSH_ASKPASS to THIS file
 export SSH_ASKPASS=$0
-# ダミーを設定
+
+# A dummy DISPLAY
 export DISPLAY=dummy:0
 
-# SSH接続 & リモートコマンド実行
-exec setsid ssh ${SSH_USER}@${SSH_HOST} ${REMOTE_CMD_CPU}>${LOG_PATH}/${SSH_HOSTNAME}-cpu-${TIMESTAMP}.log
-exec setsid ssh ${SSH_USER}@${SSH_HOST} ${REMOTE_CMD_MEMORY}>${LOG_PATH}/${SSH_HOSTNAME}-memory-${TIMESTAMP}.log
-# exec setsid ssh $SSH_USER@$SSH_HOST $REMOTE_CMD_CPU > $LOG_PATH/${SSH_HOSTNAME}-cpu2-${TIMESTAMP}.log $REMOTE_CMD_MEMORY > $LOG_PATH/${SSH_HOSTNAME}-memory2-${TIMESTAMP}.log
+# Main transactions
+for i in "${!SSH_HOSTS[@]}"; do
+   echo "Logging on ${SSH_HOSTS[$i]}..."
+   mkdir -p ${LOG_PATH}
+   setsid -f ssh ${SSH_USER}@${SSH_HOSTS[$i]} ${REMOTE_CMD_CPU}>>${LOG_PATH}/vpn-${SSH_HOSTNAMES[$i]}-cpu.log
+   setsid -f ssh ${SSH_USER}@${SSH_HOSTS[$i]} ${REMOTE_CMD_MEMORY}>>${LOG_PATH}/vpn-${SSH_HOSTNAMES[$i]}-memory.log
+   echo "Logged out ${SSH_HOSTS[$i]}..."
+done
