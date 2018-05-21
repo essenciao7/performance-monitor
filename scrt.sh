050797a6c28f3f3084d7b97bc081668d551bf841
Jingxuan Wei <joel@jp.ibm.com>
2018-05-21 10:01:47 +0900

    3rd commit

--- scrt.sh
+++ scrt.sh
@@ -3,18 +3,20 @@
 # Create timestamp for apending filenames
 TIMESTAMP=$(date +"%Y%m%d")
 
-# SSH Connection Info
+#!/bin/bash
+
+# SSH接続情報、本番環境
 SSH_USER=root
 SSH_PASS=default
-SSH_HOSTNAMES=('AFTVP03' 'AFTVP04')
-SSH_HOSTS=('10.128.1.145' '10.128.1.145')
+SSH_HOSTNAMES=('AFTVP03')
+SSH_HOSTS=('10.128.1.145')
 
-# Specify Log Directory
+# ログの保存場所
 LOG_PATH=~/Desktop/var/log
 
-# Commands for checking cpu & memory usages
-REMOTE_CMD_CPU="date; tmsh show sys cpu"
-REMOTE_CMD_MEMORY="date; tmsh show sys memory"
+# CPUやメモリ利用率を取得するためのリモートコマンド
+REMOTE_CMD_CPU="tmsh show sys cpu | grep Util% | awk '{print $8}'"
+REMOTE_CMD_MEMORY="tmsh show sys memory"
 
 
 # 後述のSSH_ASKPASSで設定したプログラム(本ファイル自身)が返す内容
@@ -23,20 +25,16 @@ if [ -n "$PASSWORD" ]; then
   exit 0
 fi
 
-# Prepare a PASSWORD for the SSH_ASKPASS-shell
+# パスワードを渡すための変数を設定し、本ファイルをSSH_ASKPASSに設定する
 export PASSWORD=$SSH_PASS
-
-# Set SSH_ASKPASS to THIS file
 export SSH_ASKPASS=$0
 
-# A dummy DISPLAY
+# ダミーなDISPLAY
 export DISPLAY=dummy:0
 
-# Main transactions
+# 性能情報取得のメイン処理
 for i in "${!SSH_HOSTS[@]}"; do
-   echo "Logging on ${SSH_HOSTS[$i]}..."
    mkdir -p ${LOG_PATH}
-   setsid -f ssh ${SSH_USER}@${SSH_HOSTS[$i]} ${REMOTE_CMD_CPU}>>${LOG_PATH}/vpn-${SSH_HOSTNAMES[$i]}-cpu.log
-   setsid -f ssh ${SSH_USER}@${SSH_HOSTS[$i]} ${REMOTE_CMD_MEMORY}>>${LOG_PATH}/vpn-${SSH_HOSTNAMES[$i]}-memory.log
-   echo "Logged out ${SSH_HOSTS[$i]}..."
+   setsid -f ssh ${SSH_USER}@${SSH_HOSTS[$i]} ${REMOTE_CMD_CPU}>>${LOG_PATH}/vpn-${SSH_HOSTNAMES[$i]}-cpu
+   setsid -f ssh ${SSH_USER}@${SSH_HOSTS[$i]} ${REMOTE_CMD_MEMORY}>>${LOG_PATH}/vpn-${SSH_HOSTNAMES[$i]}-memory
 done
