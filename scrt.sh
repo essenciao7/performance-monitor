b82ccdcf13d4634fd0bcad707939b79a4557b36b
Jingxuan Wei <joel@jp.ibm.com>
2018-05-31 21:34:25 +0900

    RC2

--- scrt.sh
+++ scrt.sh
@@ -5,25 +5,28 @@ TIMESTAMP=$(date +"%b %d %H:%M:%S")
 # 設定ファイルを読み込み
 source /Users/j367557/Documents/workspace/apm/vpnrsc.conf
 
-# 後述のSSH_ASKPASSで設定したプログラム(本ファイル自身)が返す内容
-if [ -n "$PASSWORD" ]; then
-  cat <<< "$PASSWORD"
-  exit 0
-fi
-
-# パスワードを渡すための変数を設定し、本ファイルをSSH_ASKPASSに設定する
-export PASSWORD=$SSH_PASS
-export SSH_ASKPASS=$0
-
-# ダミーなDISPLAY
-export DISPLAY=dummy:0
 
 # 性能情報取得のメイン処理
 for i in "${!SSH_HOSTS[@]}"; do
+
+  # 後述のSSH_ASKPASSで設定したプログラム(本ファイル自身)が返す内容
+  if [ -n "$PASSWORD" ]; then
+    cat <<< "$PASSWORD"
+    exit 0
+  fi
+
+  # パスワードを渡すための変数を設定し、本ファイルをSSH_ASKPASSに設定する
+  export PASSWORD=${SSH_PASS[$i]}
+  export SSH_ASKPASS=$0
+
+  # ダミーなDISPLAY
+  export DISPLAY=dummy:0
+
   mkdir -p ${LOG_PATH}
   CPU_USAGE=($(setsid -f ssh ${SSH_USER}@${SSH_HOSTS[$i]} ${REMOTE_CMD_CPU} | awk 'NR==6 {print $2}'))
   MEM_USAGE=($(setsid -f ssh ${SSH_USER}@${SSH_HOSTS[$i]} ${REMOTE_CMD_MEMORY} | awk 'NR==6 {print $4}'))
-
+  unset PASSWORD
+  
 # CPUログレベルの設定
   if [ $CPU_USAGE -lt $CPU_LOG_LEVEL_M ]; then
     CPU_LOG_LEVEL=N
